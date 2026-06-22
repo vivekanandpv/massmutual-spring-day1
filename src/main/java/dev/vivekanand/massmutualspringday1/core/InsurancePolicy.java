0c82c2046ccb9c0f395631b30aeea47f67b834a4
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-22 18:26:42 +0530

    Injecting dynamic values using SpEL. Working with environment variables.

--- src/main/java/dev/vivekanand/massmutualspringday1/core/InsurancePolicy.java
+++ src/main/java/dev/vivekanand/massmutualspringday1/core/InsurancePolicy.java
@@ -5,23 +5,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InsurancePolicy implements FinancialInstrument {
     private static final Logger LOGGER = LoggerFactory.getLogger(InsurancePolicy.class);
 
-    private PaymentProcessor paymentProcessor;
+    private final PaymentProcessor paymentProcessor;
+    private final String regionCode;
+
+    public InsurancePolicy(
+            PaymentProcessor paymentProcessor,
+            @Value("${payment.provider.region}") String region
+    ) {
+        this.paymentProcessor = paymentProcessor;
+        this.regionCode = region;
+    }
 
     @Override
     public void pay() {
-        LOGGER.info("Processing the payment");
+        LOGGER.info("Processing the payment for: " + regionCode);
         paymentProcessor.processPayment(1000);
         LOGGER.info("Finished processing the payment");
-    }
-
-    @Autowired
-    public void setPaymentProcessor(PaymentProcessor paymentProcessor) {
-        this.paymentProcessor = paymentProcessor;
     }
 }
