61b764ff132c9f7a5f5863df64ce6ed45139f8c4
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-22 16:00:25 +0530

    using the collaborator

--- src/main/java/dev/vivekanand/massmutualspringday1/MassmutualSpringDay1Application.java
+++ src/main/java/dev/vivekanand/massmutualspringday1/MassmutualSpringDay1Application.java
@@ -1,5 +1,7 @@
 package dev.vivekanand.massmutualspringday1;
 
+import dev.vivekanand.massmutualspringday1.collab.PaymentProcessor;
+import dev.vivekanand.massmutualspringday1.collab.StripePaymentProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,10 @@ public class MassmutualSpringDay1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(MassmutualSpringDay1Application.class, args);
+
+        PaymentProcessor paymentProcessor = new StripePaymentProcessor();
+        boolean paymentStatus = paymentProcessor.processPayment(100.0);
+        System.out.println("Payment status: " + paymentStatus);
     }
 
 }
