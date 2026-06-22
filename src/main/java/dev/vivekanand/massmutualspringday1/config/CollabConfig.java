d244c3e9bac41b1783f7fc56ef19d94d2be5d30a
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-22 16:44:32 +0530

    NoUniqueBeanDefinitionException

--- src/main/java/dev/vivekanand/massmutualspringday1/config/CollabConfig.java
+++ src/main/java/dev/vivekanand/massmutualspringday1/config/CollabConfig.java
@@ -2,6 +2,7 @@ package dev.vivekanand.massmutualspringday1.config;
 
 import dev.vivekanand.massmutualspringday1.collab.PaymentProcessor;
 import dev.vivekanand.massmutualspringday1.collab.StripePaymentProcessor;
+import dev.vivekanand.massmutualspringday1.collab.UpiPaymentProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,10 @@ public class CollabConfig {
     @Bean
     public PaymentProcessor provideStripe() {
         return new StripePaymentProcessor();
+    }
+
+    @Bean
+    public PaymentProcessor provideUpi() {
+        return new UpiPaymentProcessor();
     }
 }
