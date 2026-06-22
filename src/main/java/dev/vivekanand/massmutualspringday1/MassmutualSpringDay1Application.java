69c46353361a7f28bf0b532f6d84c542379c8fc3
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-22 16:30:36 +0530

    application context

--- src/main/java/dev/vivekanand/massmutualspringday1/MassmutualSpringDay1Application.java
+++ src/main/java/dev/vivekanand/massmutualspringday1/MassmutualSpringDay1Application.java
@@ -4,14 +4,15 @@ import dev.vivekanand.massmutualspringday1.collab.PaymentProcessor;
 import dev.vivekanand.massmutualspringday1.collab.StripePaymentProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MassmutualSpringDay1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MassmutualSpringDay1Application.class, args);
+        ApplicationContext context = SpringApplication.run(MassmutualSpringDay1Application.class, args);
 
-        PaymentProcessor paymentProcessor = new StripePaymentProcessor();
+        PaymentProcessor paymentProcessor = context.getBean(PaymentProcessor.class);
         boolean paymentStatus = paymentProcessor.processPayment(100.0);
         System.out.println("Payment status: " + paymentStatus);
     }
